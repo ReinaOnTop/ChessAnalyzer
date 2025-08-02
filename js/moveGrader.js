11da117e2930cee48c1e14f6b7d90d4fcf79c389
ReinaOnTop <onty39969@gmail.com>
2025-08-02 12:53:56 +0800

    Delete js/moveGrader.js

--- js/moveGrader.js
+++
@@ -1,8 +0,0 @@
-export function getMoveCategory(scoreDiff) {
-  if (scoreDiff >= 300) return 'Brilliant';
-  if (scoreDiff >= 100) return 'Great';
-  if (scoreDiff >= 30) return 'Good';
-  if (scoreDiff > -30) return 'Inaccuracy';
-  if (scoreDiff > -100) return 'Mistake';
-  return 'Blunder';
-}
